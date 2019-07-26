962c06e9ba58b9f74db1ff0bc66f7dd4d408b51c
radui1398 <radui1398@gmail.com>
2019-07-26 15:11:30 +0300

    New Structure

---
+++ frame/admin.php
@@ -0,0 +1,56 @@
+<?php
+
+add_action('admin_menu', 'cart_setup_menu');
+
+function cart_setup_menu(){
+
+add_submenu_page( 'options-general.php','RVeryCart', 'Cart Options', 'manage_options', 'cart-options', 'cart_init' );
+
+}
+
+
+
+function cart_init(){
+
+$cssStyle = fopen(plugins_url('/style.css', __FILE__), "r") or die("Unable to open file!");
+
+$cssContent = fread($cssStyle,10000);
+
+fclose($cssStyle);
+
+?>
+
+<div>
+
+   <?php screen_icon(); ?>
+
+   <h2>RVeryCart</h2>
+
+   <h3>Info:</h3>
+
+   <p> To use this plugin enable your theme support - add_theme_support('woocommerce'); </p>
+   <p> To use the "add to cart" button with RVeryCart you need to edit your button. You can do this in the
+      add-to-cart.php file from the woocommerce templates.</p>
+   <p> New <a> must respect this structure: </p>
+   <p>
+      <pre><a data-product_id="[POST_ID]" class="[add-to-cart] [product_type] [Any other class but not add_to_cart_button]" data-quantity="[quantity]" [*]>[*]</a></pre>
+   </p>
+
+
+   <form method="post" action="<?php echo plugins_url('/utils/style_updater.php', __FILE__)?>">
+
+      <h3>Cart Style</h3>
+
+      <p>Edit the cart style.</p>
+
+      <input type="hidden" name="fileLocation" value="<?php echo plugin_dir_path( __FILE__ ).'/style.css';?>" />
+
+      <textarea rows="20" id="cssEditor" style="width: 40%" name="cssEditor"><?php echo $cssContent; ?></textarea>
+
+
+
+      <?php  submit_button(); ?>
+
+   </form>
+
+</div>
---
+++ frame/settings.php
@@ -0,0 +1,63 @@
+<?php
+
+function add_localize_scriptR()
+
+{
+
+?>
+
+<script type="text/javascript">
+var jsHomeUrl = '<?php echo home_url(); ?>';
+
+var ajaxUrl = "<?php echo admin_url('admin-ajax.php') ?>";
+
+var priceCurrency = "<?php echo get_woocommerce_currency_symbol() ?>";
+</script>
+
+<?php
+
+}
+
+add_action('wp_head', 'add_localize_scriptR', 999);
+
+
+
+
+
+// register jquery and style on initialization
+
+add_action('init', 'register_plugin_script');
+
+function register_plugin_script() {
+
+    wp_register_script( 'noty', plugins_url('/js/noty.min.js', __FILE__), array('jquery'), '2.5.1' );
+
+    wp_register_script( 'rVeryCartJS', plugins_url('/js/cart_js.js', __FILE__), array('jquery'), '2.5.1' );
+
+    wp_register_style( 'notyCSS', plugins_url('/css/noty.css', __FILE__), false, '1.0.0', 'all');
+
+    wp_register_style( 'rVeryCaryCSS', plugins_url('/style.css', __FILE__), false, '1.0.0', 'all');
+
+}
+
+
+
+// use the registered jquery and style above
+
+add_action('wp_enqueue_scripts', 'enqueue_plugin_style');
+
+
+
+function enqueue_plugin_style(){
+
+   wp_enqueue_script('rVeryCartJS');
+
+   wp_enqueue_script('noty');
+
+
+
+   wp_enqueue_style( 'rVeryCaryCSS' );
+
+   wp_enqueue_style( 'notyCSS' );
+
+}
