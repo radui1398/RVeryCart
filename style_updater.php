fa62d6fe67e1640b9f20d8dbb594d7d7fa45a872
radui1398 <radui1398@gmail.com>
2019-07-26 14:30:40 +0300

    Structure

--- style_updater.php
+++ style_updater.php
@@ -1,10 +1,16 @@
-<?php
-
-   if(isset($_POST)){
-      if(isset($_POST['cssEditor'])){
-         $file = $_POST['fileLocation'];
-         file_put_contents($file, $_POST['cssEditor']);
-         header("Location: " . $_SERVER["HTTP_REFERER"]);
-         die();
-      }
+<?php
+   if(isset($_POST)){
+
+      if(isset($_POST['cssEditor'])){
+
+         $file = $_POST['fileLocation'];
+
+         file_put_contents($file, $_POST['cssEditor']);
+
+         header("Location: " . $_SERVER["HTTP_REFERER"]);
+
+         die();
+
+      }
+
 }
